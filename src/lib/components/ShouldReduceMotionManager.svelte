418fe124628880e64f975db10138a9f859344c16
Darius Cepulis <de.cepulis@gmail.com>
2024-04-05 08:14:50 -0500

    Scaffold

--- src/lib/components/ShouldReduceMotionManager.svelte
+++ src/lib/components/ShouldReduceMotionManager.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import ui from '$lib/stores/ui.svelte';
 	import { onMount } from 'svelte';
-	import { reduceMotion } from '$lib/stores';
+
 	onMount(() => {
 		const reduceQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
-		$reduceMotion = reduceQuery.matches;
+		ui.shouldReduceMotion = reduceQuery.matches;
 
-		const onQueryChange = (e: MediaQueryListEvent) => ($reduceMotion = e.matches);
+		const onQueryChange = (e: MediaQueryListEvent) => (ui.shouldReduceMotion = e.matches);
 		reduceQuery.addEventListener('change', onQueryChange);
 
 		return () => reduceQuery.removeEventListener('change', onQueryChange);
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-import autoprefixer from 'autoprefixer';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [
-		vitePreprocess({
-			postcss: {
-				plugins: [autoprefixer]
-			}
-		})
-	],
+	preprocess: vitePreprocess(),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
