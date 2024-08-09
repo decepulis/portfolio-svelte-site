b255408f6293cda80c16b0c6d4dd6606b507ec99
Darius Cepulis <dcepulis@mux.com>
2024-08-09 09:12:08 -0500

    fix base url

--- src/lib/stores/baseUrl.ts
+++ src/lib/stores/baseUrl.ts
@@ -1,4 +1,4 @@
-const baseUrl = import.meta.env.VITE_VERCEL_URL
-	? `https://${import.meta.env.VITE_VERCEL_URL}`
+const baseUrl = import.meta.env.PUBLIC_VERCEL_URL
+	? `https://${import.meta.env.PUBLIC_VERCEL_URL}`
 	: 'http://localhost:5173';
 export default baseUrl;
